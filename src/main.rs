20cc589f7a859f274102c214fa6ccb5e7c232c59
Paul Jarrett <jarrett.paul.young@gmail.com>
2023-07-02 15:32:06 -0400

    Finishes sketching out the regex search.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,112 @@
+use ignore::{DirEntry, WalkBuilder};
+use regex::Regex;
+use serde::{Deserialize, Serialize};
+use serde_json::Result;
+use std::io::BufRead;
+use std::path::Path;
+use std::{fs::File, io::BufReader};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Specific {
+    targets: Vec<String>,
+    patterns: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Configuration {
+    /// Used to mark a line as being overridden.
+    r#override: String,
+
+    /// Patterns which apply everywhere.
+    everywhere: Vec<String>,
+
+    /// Patterns which only apply in specific directories, or to specific files
+    specific: Vec<Specific>,
+}
+
+fn make_patterns(v: &Vec<String>) -> Vec<Regex> {
+    let mut regexes = Vec::new();
+    for s in v {
+        match Regex::new(s.as_str()) {
+            Ok(r) => {
+                regexes.push(r);
+            }
+            Err(error) => {
+                eprintln!("Error creating regex: {:?}", error);
+            }
+        }
+    }
+    return regexes;
+}
+
+fn check_file(entry: DirEntry, regexes: &Vec<Regex>) -> bool {
+    match entry.file_type() {
+        Some(t) => {
+            if t.is_file() {
+                if let Ok(file) = File::open(entry.path()) {
+                    let reader = BufReader::new(file);
+                    for maybe_line in reader.lines() {
+                        if let Ok(line) = maybe_line {
+                            for r in regexes {
+                                if r.is_match(line.as_str()) {
+                                    println!("Prohibited value found: {:?}", r.as_str());
+                                    return false;
+                                }
+                            }
+                        }
+                    }
+                    return true;
+                }
+                return false;
+            }
+            return false;
+        }
+        None => false,
+    }
+}
+
+fn check_specific(specific: &Specific, common_patterns: &Vec<Regex>) -> bool {
+    let mut extra_patterns = make_patterns(&specific.patterns);
+
+    for r in common_patterns {
+        extra_patterns.push(r.clone());
+    }
+
+    let mut success = true;
+    for dir in &specific.targets {
+        let path = Path::new(dir.as_str());
+        let walk = WalkBuilder::new(path).build();
+        for d in walk {
+            match d {
+                Ok(entry) => {
+                    success &= check_file(entry, &extra_patterns);
+                }
+                Err(err) => {
+                    println!("Error {:?}", err);
+                    success = false;
+                }
+            }
+        }
+    }
+    return true;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let path = Path::new(r"D:\dev\games\paperspaceships\scripts\prohibited.json");
+    if let Ok(file) = File::open(path) {
+        let reader = BufReader::new(file);
+        let read_result: Result<Configuration> = serde_json::from_reader(reader);
+
+        // Read the configuration.
+        if let Ok(configuration) = read_result {
+            let common_patterns = make_patterns(&configuration.everywhere);
+
+            // Evaluate every specific target.
+            for specific in configuration.specific {
+                check_specific(&specific, &common_patterns);
+            }
+        }
+    } else {
+        println!("Could not open file {:?}", path);
+    }
 }
